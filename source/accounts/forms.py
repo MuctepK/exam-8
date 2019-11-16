28fb1ef7033ce3334924f209cfc9a7f6613093fa
Kutman <muctep.k@gmail.com>
2019-11-16 13:39:59 +0600

    Добавлена регистрация

---
+++ source/accounts/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+from accounts.models import Profile
+
+
+class SignUpForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    email = forms.CharField(label='Email', required=True)
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+            raise ValidationError("Такой пользователь уже есть", code='username_taken')
+        except User.DoesNotExist:
+            return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        try:
+            User.objects.get(email=email)
+            raise ValidationError("Эта почта уже используется", code='email_taken')
+        except User.DoesNotExist:
+            return email
+
+    def clean(self):
+        super().clean()
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
+        labels = {'username': 'Логин', 'first_name': 'Имя', 'last_name': 'Фамилия'}
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from accounts.forms import SignUpForm
+from accounts.models import Profile
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = SignUpForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user)
+            login(request, user)
+            return redirect('webapp:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', context={'form': form})
