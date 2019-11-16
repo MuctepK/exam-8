9cc137ecd3ae8f5147bbf89e9bebbec3de5adc27
Kutman <muctep.k@gmail.com>
2019-11-16 11:34:02 +0600

    Создал модель Товар

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+CATEGORY_CHOICES = [
+    ('other', 'Другое'),
+    ('kitchen', 'Кухня'),
+    ('electronics', 'Электроника'),
+    ('children', 'Дети')
+]
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Название')
+    category = models.CharField(verbose_name='Категория', max_length=64, choices=CATEGORY_CHOICES,
+                                default=CATEGORY_CHOICES[0][0])
+    description = models.TextField(null=True, blank=True, verbose_name='Описание', max_length=512)
+    img = models.ImageField(verbose_name='Картинка', null=True, blank=True, upload_to='product_pics')
